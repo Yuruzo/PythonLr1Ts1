4b4ab35ee84c107e8c18c1c7ad9ede728b003edf
_Misha_ <_Misha_@DESKTOP-MICHAEL>
2025-03-02 16:51:33 +0300

    Добавлена функция 3: произведение максимального числа, не взаимно простого с данным, и суммы цифр, меньших 5

--- main.py
+++ main.py
@@ -15,3 +15,24 @@ def max_digit_not_divisible_by_three(n):
         if digit_int % 3 != 0 and digit_int > max_digit:
             max_digit = digit_int
     return max_digit
+
+def product_max_not_coprime_and_sum_digits_less_than_five(n):
+    def smallest_divisor(n):
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return i
+        return n
+
+    def sum_digits_less_than_five(n):
+        return sum(int(digit) for digit in str(n) if int(digit) < 5)
+
+    min_divisor = smallest_divisor(n)
+    max_not_coprime = -1
+
+    for i in range(2, n):
+        if math.gcd(n, i) != 1 and i % min_divisor != 0:
+            if i > max_not_coprime:
+                max_not_coprime = i
+
+    sum_digits = sum_digits_less_than_five(n)
+    return max_not_coprime * sum_digits
