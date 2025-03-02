92336a92626d78073972e5067335af50ccbbc1e0
_Misha_ <_Misha_@DESKTOP-MICHAEL>
2025-03-02 18:04:41 +0300

    Задание 15: Добавлена функция для вывода индексов массива в порядке, соответствующем значениям.

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 
-def sort_by_ascii_triple_deviation(words):
+def print_array_indices(arr):
 
-    # Задание 14: Сортировка строк по квадратичному отклонению дисперсии максимального среднего веса ASCII-кода тройки символов.
-    # :param words: Список строк.
-    # :return: Отсортированный список строк.
+    # Задание 15: Вывод индексов массива в порядке, соответствующем значениям.
+    # :param arr: Целочисленный массив.
+    # :return: Список индексов.
 
-    def ascii_triple_avg(word):
-        max_avg = 0
-        for i in range(len(word) - 2):
-            avg = (ord(word[i]) + ord(word[i+1]) + ord(word[i+2])) / 3
-            if avg > max_avg:
-                max_avg = avg
-        return max_avg
-    first_max_avg = ascii_triple_avg(words[0])
-    def deviation(word):
-        return (ascii_triple_avg(word) - first_max_avg) ** 2
-    return sorted(words, key=deviation)
+    return sorted(range(len(arr)), key=lambda i: arr[i])
 
-words = ["hello", "world", "python", "programming", "algorithm"]
-print("Отсортированные строки:", sort_by_ascii_triple_deviation(words))
+arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+print("Индексы массива:", print_array_indices(arr))
